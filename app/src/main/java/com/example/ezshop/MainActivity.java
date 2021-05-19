ac4de3a0b8733b94edc6c2b6b18f7a9d0e8aab57
Fares <fares9950@gmail.com>
2021-05-19 05:08:21 +0200

    added login page xml and login java code

--- app/src/main/java/com/example/ezshop/MainActivity.java
+++ app/src/main/java/com/example/ezshop/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.ezshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText Name;
+    private  EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int Count = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Info = (TextView)findViewById(R.id.tvInfo);
+        Login = (Button)findViewById(R.id.btnLogin);
+
+        Info.setText("No of attempts remaining: 5");
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+    }
+
+    private void  Validate(String username, String password){
+        if ((username.equals("Admin")) && (password.equals("1234"))){
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }
+        else{
+            Count--;
+            Info.setText("No of attempts remaining: " + String.valueOf(Count));
+            if(Count == 0){
+                Login.setEnabled(false);
+            }
+        }
+
     }
 }
