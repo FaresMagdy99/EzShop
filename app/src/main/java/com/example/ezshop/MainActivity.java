e952026bcfffa5d0477b6e59135197c4c5277c20
Fares <fares9950@gmail.com>
2021-05-29 23:20:22 +0200

    added a new comment

--- app/src/main/java/com/example/ezshop/MainActivity.java
+++ app/src/main/java/com/example/ezshop/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView Info;
     private Button Login;
     private int Count = 5;
-
+    // add comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
