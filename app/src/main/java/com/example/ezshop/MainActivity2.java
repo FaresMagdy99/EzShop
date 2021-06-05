a425062e27d72aef50e3857c846e015ee87c1ab6
Fares <fares9950@gmail.com>
2021-06-05 23:55:41 +0200

    added scroll view and open new activity onClick on card

--- app/src/main/java/com/example/ezshop/MainActivity2.java
+++ app/src/main/java/com/example/ezshop/MainActivity2.java
@@ -1,14 +1,67 @@
 package com.example.ezshop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    GridLayout mainGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        mainGrid = (GridLayout) findViewById(R.id.mainGrid);
+
+        setSingleEvent(mainGrid);
+    }
+
+    private void setSingleEvent(GridLayout mainGrid) {
+        for(int i = 0 ;i<mainGrid.getChildCount(); i++){
+            CardView cardView = (CardView)mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    if(finalI == 0){
+                        Intent intent = new Intent(MainActivity2.this,FirstActivity.class);
+                        startActivity(intent);
+                    }
+                    else if(finalI == 1){
+                        Intent intent = new Intent(MainActivity2.this,SecondActivity.class);
+                        startActivity(intent);
+                    }
+                    else if(finalI == 2){
+                        Intent intent = new Intent(MainActivity2.this,ThirdActivity.class);
+                        startActivity(intent);
+                    }
+                    else if(finalI == 3){
+                        Intent intent = new Intent(MainActivity2.this,FourthActivity.class);
+                        startActivity(intent);
+                    }
+                    else if(finalI == 4){
+                        Intent intent = new Intent(MainActivity2.this,FifthActivity.class);
+                        startActivity(intent);
+                    }
+                    else if(finalI == 5){
+                        Intent intent = new Intent(MainActivity2.this,SixthActivity.class);
+                        startActivity(intent);
+                    }
+
+                    else {
+                        Toast.makeText(MainActivity2.this, "Please set activity for this card item" ,Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
+        }
     }
 }
