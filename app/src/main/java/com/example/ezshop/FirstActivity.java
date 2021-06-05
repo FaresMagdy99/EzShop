64a5a3dcaabd4a022314b7d6520ccaed671e95ea
Fares <fares9950@gmail.com>
2021-06-06 00:24:54 +0200

    added menu for each category

--- app/src/main/java/com/example/ezshop/FirstActivity.java
+++ app/src/main/java/com/example/ezshop/FirstActivity.java
@@ -1,14 +1,79 @@
 package com.example.ezshop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
+    GridLayout mainGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        mainGrid = (GridLayout) findViewById(R.id.firstmainGrid);
+
+        setSingleEvent(mainGrid);
+    }
+
+    private void setSingleEvent(GridLayout mainGrid) {
+        for(int i = 0 ;i<mainGrid.getChildCount(); i++){
+            CardView cardView = (CardView)mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    if(finalI == 0){
+                        Intent intent = new Intent(FirstActivity.this,ItemViewActivity.class);
+                        startActivity(intent);
+                    }
+                    else if(finalI == 1){
+                        Intent intent = new Intent(FirstActivity.this,ItemViewActivity.class);
+                        startActivity(intent);
+                    }
+                    else if(finalI == 2){
+                        Intent intent = new Intent(FirstActivity.this,ItemViewActivity.class);
+                        startActivity(intent);
+                    }
+                    else if(finalI == 3){
+                        Intent intent = new Intent(FirstActivity.this,ItemViewActivity.class);
+                        startActivity(intent);
+                    }
+                    else if(finalI == 4){
+                        Intent intent = new Intent(FirstActivity.this,ItemViewActivity.class);
+                        startActivity(intent);
+                    }
+                    else if(finalI == 5){
+                        Intent intent = new Intent(FirstActivity.this,ItemViewActivity.class);
+                        startActivity(intent);
+                    }
+                    else if(finalI == 6){
+                        Intent intent = new Intent(FirstActivity.this,ItemViewActivity.class);
+                        startActivity(intent);
+                    }
+                    else if(finalI == 7){
+                        Intent intent = new Intent(FirstActivity.this,ItemViewActivity.class);
+                        startActivity(intent);
+                    }
+                    else if(finalI == 8){
+                        Intent intent = new Intent(FirstActivity.this,ItemViewActivity.class);
+                        startActivity(intent);
+                    }
+
+                    else {
+                        Intent intent = new Intent(FirstActivity.this,ItemViewActivity.class);
+                        startActivity(intent);
+                    }
+
+                }
+            });
+        }
     }
 }
